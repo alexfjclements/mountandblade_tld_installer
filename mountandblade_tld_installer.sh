b5dc61e16ce470c5976a6b2dc1218e9eaa4c07d9
Alex <Alexander.Clements@rbs.co.uk>
2019-09-30 11:16:57 +0100

    slight refactor of the bash script for readability

--- mountandblade_tld_installer.sh
+++ mountandblade_tld_installer.sh
@@ -11,17 +11,21 @@ if [ "$answer" == "y" ]
 then
   echo Downloading the unar command-line utility...
   cd /tmp && curl -L 'cdn.theunarchiver.com/downloads/unarMac.zip' -o unar.zip && unzip -o unar.zip
+
   echo Removing any existing TLD folder...
   cd ~/'Library/Application Support/Steam/steamapps/common/MountBlade Warband/Modules/'
   rm -rf TLD
+
   echo Extracting TLD and the nightly patch...
   cd ~/Downloads
   /tmp/unar -D -f -o ~/'Library/Application Support/Steam/steamapps/common/MountBlade Warband/Modules' TLD_3.5_wb.7z
   /tmp/unar -D -f -o ~/'Library/Application Support/Steam/steamapps/common/MountBlade Warband/Modules' `find . -name 'TLD_3.5_wb_*' | sort -r -u | head -n 1`
+
   echo Fixing the font and collision bodies...
   cd ~/'Library/Application Support/Steam/steamapps/common/MountBlade Warband/Modules/TLD/' && cp -f ./Textures/font.dds ../../Textures/
   cp -f ./Data/font_data.xml ../../Data/
   cp -f ./Data/skeleton_bodies.xml ../../Data/
+  
   echo Fixing corrupted sound files...
   cd /tmp && curl -L 'github.com/tldmod/tldmod/releases/download/TLD3.3REL/TLD_3.5_Sounds.zip' -o TLD_3.5_Sounds.zip && unzip TLD_3.5_Sounds.zip -d ~/'Library/Application Support/Steam/steamapps/common/MountBlade Warband/Modules/TLD/'
 fi
